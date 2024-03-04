dbb6788b3771fc406cb1eefd1e81c8c9a11387c4
Hendra Putu <mchen.putu@gmail.com>
2024-03-04 13:35:39 +0800

    import tours data from json

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,52 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const Tour = require('../../models/tourModel');
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<password>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB Connection successful!'));
+
+// read json file
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'),
+);
+
+// import data into db
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data successfully loaded!');
+  } catch (error) {
+    console.log(error);
+  }
+  process.exit();
+};
+
+// delete all data from db
+const deleteAllData = async () => {
+  try {
+    await Tour.deleteMany();
+  } catch (error) {
+    console.log(error);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteAllData();
+}
