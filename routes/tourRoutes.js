11a37f47044078b62808bd8897c14806a47cc5b2
Hendra Putu <mchen.putu@gmail.com>
2024-03-07 13:03:41 +0800

    add aggregation pipleine: unwinding and projecting

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -11,6 +11,7 @@ router
   .get(tourController.aliasTopTours, tourController.getAllTours);
 
 router.route('/tour-stats').get(tourController.getTourStats);
+router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
 
 router
   .route('/')
