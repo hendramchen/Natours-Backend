9c7a4345c31c3c5b50bcbcfbe07207db2d737e6f
Hendra Putu <mchen.putu@gmail.com>
2024-06-17 13:54:44 +0800

    modeling mongodb data and import new data

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -20,9 +20,7 @@ mongoose
   .then(() => console.log('DB Connection successful!'));
 
 // read json file
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'),
-);
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
 
 // import data into db
 const importData = async () => {
